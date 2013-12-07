5347e90b2b13edb46ae3747f01a80ec5c4d57166
pavelperkin <perkinpv@mail.ru>
2013-12-07 16:56:36 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DnTaxi::Application.config.secret_key_base = 'd5ea538cdbaba2d702bbbe93d286de35258bbf7154b29d00ac8726e2d293015cd6cc3dc3f3c35ee78602c4555cf90d724eee991449c866db03b1396ed6bc17f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DnTaxi::Application.config.session_store :cookie_store, key: '_dn_taxi_session'
